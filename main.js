a820b4a2323e9d7fd00716775344955aaef2fd11
Ahmed Boussadia <ahmed.boussadia@nexworld.fr>
2016-01-12 09:00:24 +0100

    Adding command line scripts to project : csv scrapper

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var json2csv = require("json2csv"),
-	fs = require('fs');;
-
 /*var NexSportsFrScraper = require('./NexSportsFrScraper');
 
 var next = function(){
@@ -32,8 +29,6 @@ var next = function(){
 };
 
 NexSportsFrScraper.getAndSaveTeams(next);*/
-
-var MPGScraper = require('./MPGScraper/scraper');
 //var MPGCrawler = require('./MPGScraper/crawler');
 //MPGScraper.getStatisticsFrom('http://www.monpetitgazon.com/DetailMatchChampionnat2.php?idmatch=805452');
 //MPGScraper.getResultTableFromFixtureUrl("http://www.monpetitgazon.com/calendrier-resultat-championnat.php?num=16");
@@ -41,80 +36,3 @@ var MPGScraper = require('./MPGScraper/scraper');
 //MPGScraper.getPlayerStatistics('http://blog.monpetitgazon.com/player/brechet-6586/')
 
 //MPGCrawler.getAllFixturesResults();
-
-(function(initIndex, endIndex){
-	var iterGame = function(result, index, results, fixturesStats, finalCallback){
-		var scoreDetailUrl = result.scoreDetailUrl;
-		MPGScraper.call("FIXTURE_STATISTICS_PAGE", scoreDetailUrl, function(table){
-			var computedResults = [];
-			for(playerId in table){
-				var localResult = table[playerId];
-				localResult["playerId"] = playerId;
-				localResult["fixture"] = result.fixture;
-				localResult["homeTeam"] = result.homeTeam;
-				localResult["awayTeam"] = result.awayTeam;
-				computedResults.push(localResult);
-			}
-			fixturesStats = fixturesStats.concat(computedResults);
-
-			if(index+1<results.length){
-				iterGame(results[index+1], index +1, results, fixturesStats,finalCallback)
-			}else{
-				if(finalCallback) finalCallback(fixturesStats);
-			}
-		}, function(err){
-			console.log(err)
-		})
-	}
-	var iterFixtures = function(i, endIndex, globalFixturesStats){
-		var baseUrl = "http://www.monpetitgazon.com/calendrier-resultat-championnat.php?num=";
-		var url = baseUrl + i;
-		MPGScraper.call("FIXTURES_PAGE", url, function(results){
-			results.fixturesResults.forEach(function(result, index, results){
-				result.fixture = i;
-			});
-
-			iterGame(results.fixturesResults[0], 0,results.fixturesResults, globalFixturesStats,function(globalFixturesStats){
-				if(i+1<=endIndex){
-					iterFixtures(i+1,endIndex, globalFixturesStats)
-				}else{
-					var fields = [
-						"playerId",
-						"name",
-						"note",
-						"goals",
-						"goal_assist",
-						"homeTeam",
-						"awayTeam",
-						"fixture"
-					]
-
-					json2csv({data: globalFixturesStats, fields: fields, del: ";"}, function(err, csv){
-						if(!err){
-							fs.writeFile("./target/fixture_"+i+"_"+endIndex+".json", JSON.stringify(globalFixturesStats), function(err) {
-								if(err) {
-									return console.log(err);
-								}
-								console.log("The file was saved!");
-							});
-							fs.writeFile("./target/fixture_"+initIndex+"_"+endIndex+".csv", csv, function(err) {
-								if(err) {
-									return console.log(err);
-								}
-								console.log("The file was saved!");
-							});
-						}else{
-							console.log(err)
-						}
-						
-					})
-				}
-			})
-		}, function(err){
-			console.log(err, i, endIndex);
-		})
-
-	};
-
-	iterFixtures(initIndex, endIndex, []);
-})(1, 20)
---
+++ scripts/csvscraper.js
@@ -0,0 +1,105 @@
+#! /usr/bin/env node
+
+var json2csv = require("json2csv"),
+	MPGScraper = require('../MPGScraper/scraper');
+	bunyan = require('bunyan'),
+	fs = require('fs'),
+	program = require('commander');
+
+var fixtuxeStatistcisRetrieval = function(initIndex, endIndex){
+	var iterGame = function(result, index, results, fixturesStats, finalCallback){
+		var scoreDetailUrl = result.scoreDetailUrl;
+		if (scoreDetailUrl){
+			MPGScraper.call("FIXTURE_STATISTICS_PAGE", scoreDetailUrl, function(table){
+				var computedResults = [];
+				for(playerId in table){
+					var localResult = table[playerId];
+					localResult["playerId"] = playerId;
+					localResult["fixture"] = result.fixture;
+					localResult["homeTeam"] = result.homeTeam;
+					localResult["awayTeam"] = result.awayTeam;
+					computedResults.push(localResult);
+				}
+				fixturesStats = fixturesStats.concat(computedResults);
+
+				if(index+1<results.length){
+					iterGame(results[index+1], index +1, results, fixturesStats,finalCallback)
+				}else{
+					if(finalCallback) finalCallback(fixturesStats);
+				}
+			}, function(err){
+				console.log(err);
+			})
+		}else{
+			if(index+1<results.length){
+				iterGame(results[index+1], index +1, results, fixturesStats,finalCallback)
+			}else{
+				if(finalCallback) finalCallback(fixturesStats);
+			}
+		}
+		
+	}
+	var iterFixtures = function(i, endIndex, globalFixturesStats){
+		var baseUrl = "http://www.monpetitgazon.com/calendrier-resultat-championnat.php?num=";
+		var url = baseUrl + i;
+		MPGScraper.call("FIXTURES_PAGE", url, function(results){
+			results.fixturesResults.forEach(function(result, index, results){
+				result.fixture = i;
+			});
+
+			iterGame(results.fixturesResults[0], 0,results.fixturesResults, globalFixturesStats,function(globalFixturesStats){
+				if(i+1<=endIndex){
+					iterFixtures(i+1,endIndex, globalFixturesStats)
+				}else{
+					var fields = [
+						"playerId",
+						"name",
+						"note",
+						"goals",
+						"goal_assist",
+						"is_home",
+						"homeTeam",
+						"awayTeam",
+						"fixture"
+					]
+
+					json2csv({data: globalFixturesStats, fields: fields, del: ";"}, function(err, csv){
+						if(!err){
+							fs.writeFile("../target/fixture_"+initIndex+"_"+endIndex+".csv", csv, function(err) {
+								if(err) {
+									return console.log(err);
+								}
+								console.log("The file was saved!");
+							});
+						}else{
+							console.log(err)
+						}
+						
+					})
+				}
+			})
+		}, function(err){
+			console.log(err, i, endIndex);
+		})
+
+	};
+
+	iterFixtures(initIndex, endIndex, []);
+};
+
+
+program
+	.arguments('action')
+	.option('-s, --start <startIndex>', 'Première journée à récupérer')
+	.option('-e, --end <endIndex>', 'Dernière journée à récupérer')
+	.option('-f, --file <fileoutput>', 'Chemin du fichier à savegarder')
+	.action(function(action){
+		var FIXTURE_STATISTICS_PAGE = "fixtures_stats";
+		var ACTION_LIST = ["FIXTURE_STATISTICS_PAGE"];
+		if (action === FIXTURE_STATISTICS_PAGE){
+			var startIndex = parseInt(program.start);
+			var endIndex = parseInt(program.end);
+			fixtuxeStatistcisRetrieval(startIndex, endIndex);
+		}
+	})
+	.parse(process.argv);
